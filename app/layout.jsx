d2ac506dab900264a6e3e143341996ffd40b28e1
Ashique Ansari <nasiba.ansari@gmail.com>
2026-08-25 21:36:07 +0530

    add google analytics code

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,3 +1,4 @@
+import Script from "next/script";
 import "./globals.css";
 import Header, { HeaderChrome } from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx";
@@ -19,11 +20,33 @@ export const metadata = {
     "business applications",
     "Uidoyen",
   ],
+  verification: {
+    google: "68ujtA-fCziSQf6SqXWvk28oSN5TkT_aQkJa2BPJSf8",
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta
+          name="google-site-verification"
+          content="68ujtA-fCziSQf6SqXWvk28oSN5TkT_aQkJa2BPJSf8"
+        />
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-QDDD9BNQ92"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-QDDD9BNQ92');
+          `}
+        </Script>
+      </head>
       <body>
         <HeaderChrome />
         <div id="smooth-wrapper">
